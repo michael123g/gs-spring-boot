60553eae92cd3a37b416f6086456d6b3a7034cc0
Miguel Gonzales <miguel.gonzales1@telus.com>
2019-10-09 21:16:16 -0400

    Adding modification

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot From Franck!";
+        return "Greetings Spring Boot!";//new Greeter().greet(name);
     }
     
 }
