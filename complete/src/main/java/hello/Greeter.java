a35c0914f8860178bf7a3d303ee572c51dde1f30
Miguel Gonzales <miguel.gonzales1@telus.com>
2019-10-09 23:26:29 -0400

    Correcting test error try 2

---
+++ complete/src/main/java/hello/Greeter.java
@@ -0,0 +1,17 @@
+package edu.ca.ets;
+
+import static java.lang.String.format;
+
+/**
+ * Hello World!
+ * 
+ */
+
+ public class Greeter
+ {
+    public String greet(String name) {
+      System.out.println("Greet was called with param " + name);
+      return format("Greetings %s how are you doing today!", name);
+    }
+
+ }
